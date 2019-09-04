b56b5c55be9c9c18faa15fd190b1eca6df746b2b
Omar <omarreda5050@gmail.com>
2019-09-04 12:09:04 +0200

    Index
    Index

---
+++ js/main.js
@@ -0,0 +1,23 @@
+
+
+$(document).ready(function () {
+
+  ///////////////////////// WOW Animation ////////////////////////////////
+
+
+  var wow = new WOW(
+    {
+      boxClass: 'wow',      // default
+      animateClass: 'animated', // default
+      offset: 0,          // default
+      mobile: false,      // default
+    }
+  )
+  wow.init();
+
+
+
+});
+
+
+
